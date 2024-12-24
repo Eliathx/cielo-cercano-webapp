975c9c0117ed9fe7b2970586119a410b38a4e9b4
Eliath Velasco <eliath.velasco@gmail.com>
2024-12-23 19:04:36 -0500

    fixed jsx in svg

--- app/components/FilterOptions/FilterOptions.js
+++ app/components/FilterOptions/FilterOptions.js
@@ -25,7 +25,7 @@ const FilterSlider = ({ label, value, onChange, min, max, step, valueLabelFormat
 
 const CloseButton = () => {
   const {setFilterVisible } = useFilterSettings();
-  return <svg width="24px" style={{cursor:"pointer"}} onClick={()=>setFilterVisible(false)} height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor"><path d="M18 6L6 18M6 6L18 18" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+  return <svg width="24px" style={{cursor:"pointer"}} onClick={()=>setFilterVisible(false)} height="24px" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor"><path d="M18 6L6 18M6 6L18 18" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
 };
 
 export default function FilterOptions() {
